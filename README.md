dad682c029acfad63f2524793d8b9f8eb75bdcdf
Tulio Navarro <tulio.navarro@antena1.com>
2016-03-04 18:15:47 -0300

    Finalização página de lista de produtos selecionados

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Teste Front-End Walmart
 
-Criaçaõ de três páginas, sendo elas: Listagem de produtos selecionados, Endereços de entrega e
+Criação de três páginas, sendo elas: Listagem de produtos selecionados, Endereços de entrega e.
 perfil do usuário.
 
 ## Tecnologias utilizadas
 1. **Foundation (Framework CSS)**
-Escolhido por facilitar a criação de layouts respostivos e também a formatação 
-utilizando SASS, além de contrar com diversos modelos pré-prontos.
+Escolhido por facilitar a criação de layouts responsivos e também a formatação 
+utilizando SASS, além de contar com diversos modelos pré-prontos.
 
-2. ****
+2. ** AngularJs (Framework JavaScript) **
+Apesar de não dominar totalmente a tecnologia, tomei como desafio fazer este projeto colocando
+alguns conceitos de AngularJs que já tenho conhecimento. Além disso, acredito no potencial do Framework.
+
---
+++ js/app.js
@@ -0,0 +1,75 @@
+(function() {
+  var app = angular.module('walmartApp', []);
+
+  app.controller('ProdutosController', function(){
+    this.produtos = produtosLoja;
+
+    this.somaTotal = function(){
+      var totalSoma = 0;
+
+      for (var i in this.produtos){
+        if(this.produtos[i].ativo == 1)
+          totalSoma += this.produtos[i].qtd * this.produtos[i].preco;
+      }
+
+      return totalSoma;
+    };
+
+    this.total = this.somaTotal();
+
+    this.removerProduto = function(id){
+       for (var i in this.produtos){
+        if(this.produtos[i].id == id)
+          this.total -= (this.produtos[i].qtd * this.produtos[i].preco);
+      } 
+    };
+
+
+    this.finalizarCompra = function(){
+        alert('Sua Compra foi feita');
+    };
+
+
+  });
+
+  var produtosLoja = [
+      {
+        id: 1,
+        nome: 'Azurite',
+        descricao: 'Sistema Operacional: Android OS, v4.1.2 Tamanho do Display: 4.7" Câmera: 5MP GPS: SIM',
+        preco: 8.5,
+        img: "img/produtos/galaxy-win-duos.png",
+        ativo: 1,
+        qtd: 2,
+      },
+       {
+        id: 2,
+        nome: 'Azurite',
+        descricao: 'Sistema Operacional: Android OS, v4.1.2 Tamanho do Display: 4.7" Câmera: 5MP GPS: SIM',
+        preco: 8.5,
+        img: "img/produtos/galaxy-win-duos.png",
+        ativo: 1,
+        qtd: 1,
+      },
+       {
+        id: 3,
+        nome: 'Azurite',
+        descricao: 'Sistema Operacional: Android OS, v4.1.2 Tamanho do Display: 4.7" Câmera: 5MP GPS: SIM',
+        preco: 8.5,
+        img: "img/produtos/galaxy-win-duos.png",
+        ativo: 1,
+        qtd: 3,
+      },
+       {
+        id: 4,
+        nome: 'Azurite',
+        descricao: 'Sistema Operacional: Android OS, v4.1.2 Tamanho do Display: 4.7" Câmera: 5MP GPS: SIM',
+        preco: 8.5,
+        img: "img/produtos/galaxy-win-duos.png",
+        ativo: 0,
+        qtd: 1,
+      },
+
+    ];
+
+})();
